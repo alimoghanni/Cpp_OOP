d768af96ea848a7facf12a5829c28be68e6e3f53
Ali Moghanni <a.moghanni>
2021-04-20 11:47:20 +0200

    encapsulation second commit

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -1,6 +1,6 @@
 /*
  * ============================================================================
- * Cpp_OOP  --  Class Method
+ * Cpp_OOP  --  Encapsulation
  * 
  * Implementation: Ali Moghanni
  * Resources:
@@ -22,11 +22,9 @@ private:
     string lName;
     double pay;
     string email;
+
 public:
-    Employee(string empfName, string emplName, double emppay) : fName(empfName), lName(emplName), pay(emppay)
-    {
-        email = fName + "." + lName + "@email.com";
-    }
+    Employee(string empfName, string emplName, double emppay) : fName(empfName), lName(emplName), pay(emppay) {}
 
     // Member functions:
     void FullName()
@@ -40,12 +38,28 @@ public:
         cout << "First Name : " << fName << endl;
         cout << "Last Name : " << lName << endl;
         cout << "Pay : " << pay << endl;
-        cout << "Email : " << email << endl;
+        cout << "Email : " << SetEmail() << endl;
     }
 
     void ApplyRaise()
     {
         pay = pay + 100;
+    }
+
+    void GetfName()
+    {
+        cout << "First Name : " << fName << endl;
+    }
+
+    void SetfName(string empfName)
+    {
+        fName = empfName;
+    }
+
+    string SetEmail()
+    {
+        email = fName + "." + lName + "@email.com";
+        return email;
     }
 };
 
@@ -53,17 +67,17 @@ int main()
 {
 
     Employee emp1("Alex", "Kim", 45000);
-    Employee emp2("Jeni", "Tom", 59000);
 
-    
+    emp1.GetInfo();
 
-    emp1.GetInfo(); 
-
-    emp1.ApplyRaise();
+    emp1.ApplyRaise();     // change the private pay value
+    emp1.SetfName("Nick"); // set the name
 
     cout << endl;
 
-    emp1.GetInfo(); 
+    emp1.GetInfo();
+
+    emp1.FullName();
 
     return 0;
 }
